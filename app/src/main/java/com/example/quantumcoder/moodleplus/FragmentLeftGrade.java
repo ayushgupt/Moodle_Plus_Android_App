4abc3263aa23a35a4ff942089a37d22651f112d5
Manish Singh <singhmanish1997@gmail.com>
2016-02-20 20:07:09 +0530

    added profile layout

--- app/src/main/java/com/example/quantumcoder/moodleplus/FragmentLeftGrade.java
+++ app/src/main/java/com/example/quantumcoder/moodleplus/FragmentLeftGrade.java
@@ -12,6 +12,6 @@ public class FragmentLeftGrade extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.left_grade_layout,null);
+        return inflater.inflate(R.layout.threads_layout,null);
     }
 }
